a9b7752bde63b2b802b61f6a028826eb9b50c738
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-05 21:45:49 +0300

    content: dummy home page content removed

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../components/counter/counterSlice";
 import navTerminalReducer from "../components/navTerminal/navTerminalSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     visibility: navTerminalReducer,
   },
 });
