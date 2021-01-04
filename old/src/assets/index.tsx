1c1032f7abd7c8586ba632b8d7ab495ebb9cb873
Emre Sen <maemresen@yazilim.vip>
2021-01-04 20:29:35 +0300

    refactor: old files removed

--- old/src/assets/index.tsx
+++
@@ -1,7 +0,0 @@
-import linuxPenguin from './images/linux_penguin.jpg'
-import emrePortrait from './images/emre_portrait.jpeg'
-import tuxPenguin from './images/tux_penguin.png'
-import rainMatrix from './images/rain_matrix.jpg'
-
-const wallpaper = rainMatrix
-export { wallpaper, emrePortrait, linuxPenguin, tuxPenguin, rainMatrix }
