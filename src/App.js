35e92ebbc3303fa40e314b0b4ff4b8da89cf31cd
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-01-10 06:37:06 +0300

    primereact

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ import {Divider} from "primereact/divider";
 
 function App() {
     return (
-        <div className="p-grid flex align-items-center justify-content-center bg-black-alpha-70" id="app">
-            <div className="col-12 lg:col-8">
-                <Card className="bg-primary-reverse shadow-7 border-1 border-primary">
+        <div className="p-grid flex align-items-center justify-content-center select-none" id="app">
+            <div className="col-12 xl:col-6">
+                <Card className="bg-black-alpha-60 border-3 border-green-700 shadow-7">
                     <div className="grid">
-                        <div className="col-3">
-                            <img className="hidden lg:block" src={profilePic} height="130" width="130" alt="alt"/>
+                        <div className="hidden xl:block col-3">
+                            <img
+                                src="https://avatars2.githubusercontent.com/u/17517090?s=460&u=a0e442b6913f827d2e51e00abe143b2dbc375f33&v=4"
+                                height="130" width="130"
+                                alt="alt"
+                            />
                         </div>
-                        <div className="col-6 text-500">
+                        <div className="col-12 xl:col-6 text-500 text-xl">
                             <div className="text-center">
                                 <div>
                                     Mehmet Arif Emre Şen
@@ -30,38 +34,65 @@ function App() {
                                 </div>
                             </div>
                             <Divider type="dashed" layout="horizontal"/>
-                            <p className="text-justify">
+                            <p className="text-center xl:text-justify">
                                 I am an entrepreneur who wants to learn new technologies and use them to make things
                                 easier. My life goal is having my own job and creating a software community that
                                 everyone can get benefit from. I have deep interest to Object Oriented Paradigm.
                             </p>
                             <Divider type="dashed" layout="horizontal"/>
-                            <div className="flex align-items-center justify-content-between">
-                                <div>
-                                    Content 2
+                            <div className="flex flex-column align-items-center">
+                                <div className="text-green-700">
+                                    <i className="pi pi-file ml-auto mr-2"/>
+                                    <span>Check My Cv here</span>
+                                    <i className="pi pi-file ml-2 mr-auto"/>
                                 </div>
-                                <Divider type="dashed" layout="vertical"/>
-                                <div>
-                                    Content 3
+                                <div className="mt-3">
+                                    <span
+                                        className="cursor-pointer hover:text-gray-600 mr-4 underline">
+                                        English...
+                                    </span>
+                                    <span className="cursor-pointer hover:text-gray-600 underline">
+                                        Türkçe...
+                                    </span>
                                 </div>
                             </div>
                             <Divider type="dashed" layout="horizontal"/>
-                            <div className="flex justify-content-between">
-                                <i className="pi pi-file "/>
-                                <span>Check My Cv here</span>
-                                <i className="pi pi-file"/>
+                            <p className="text-center xl:text-justify">
+                                We have a software community named as YazilimVIP. Feel free to visit our website by the
+                                following link.
+                            </p>
+                            <div
+                                className="flex justify-content-center align-items-center cursor-pointer hover:text-gray-600">
+                                <i className="pi pi-file ml-auto mr-2"/>
+                                <span className="underline">
+                                    www.yazilim.vip
+                                </span>
+                                <i className="pi pi-file ml-2 mr-auto"/>
+                            </div>
+                            <Divider type="dashed" layout="horizontal"/>
+                            <div className="flex flex-column align-items-center">
+                                <div className="mx-auto mb-3">
+                                    Also, I am a Linux user.
+                                </div>
+                                <div
+                                    className="flex justify-content-center align-items-center cursor-pointer hover:text-gray-600">
+                                    <i className="pi pi-thumbs-up ml-auto mr-2"/>
+                                    <div className="underline text-center">
+                                        Preferred Distro: Manjaro XFCE
+                                    </div>
+                                    <i className="pi pi-thumbs-up ml-2 mr-auto"/>
+                                </div>
+
                             </div>
                         </div>
-                        <div className="col-3">
-                            <img className="hidden lg:block ml-auto" src={profilePic} height="130" width="130"
-                                 alt="alt"/>
+                        <div className="hidden xl:block col-3">
+                            <img className="xl:block ml-auto" src={profilePic} height="130" width="130" alt="alt"/>
                         </div>
                     </div>
                 </Card>
             </div>
         </div>
-    )
-        ;
+    );
 }
 
 export default App;
