28a5664689da22f124b07d83a5e9a28d6933b027
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-10-08 05:47:50 +0300

    wip: base structure of card updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
 import { Col, Container, Row } from 'react-bootstrap';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChampagneGlasses, faFile } from '@fortawesome/free-solid-svg-icons';
 import { COLORS } from './utils/styles';
+
+import './App.css';
 
 const TitleContainer = styled.div`
   height: 100%;
@@ -24,64 +28,128 @@ const SubTitle = styled.h3`
 const Section = styled.div`
   color: ${({ primary }) => (primary ? COLORS.PRIMARY : COLORS.SECONDARY)};
   text-align: justify;
-  
+
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
+const MaeLink = styled.a`
+  color: ${COLORS.SECONDARY};
+
+  &:hover {
+    cursor: pointer;
+    color: ${COLORS.SECONDARY_DARK} !important;
+  }
+`;
+
+const CvContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+const MainContainer = styled.div`
+  border: 3px solid ${COLORS.PRIMARY};
+  background-color: rgba(0,0,0,.3)
+`;
+
 const App = () => (
   <Container>
-    <Row>
-      <Col md={2}>
-        <Image
-          width="130"
-          rounded
-          src="https://avatars2.githubusercontent.com/u/17517090?s=460&u=a0e442b6913f827d2e51e00abe143b2dbc375f33&v=4"
-        />
-      </Col>
-      <Col md={8}>
-        <TitleContainer>
-          <Title>Mehmet Arif Emre Şen</Title>
-          <SubTitle>(a.k.a maemresen)</SubTitle>
-        </TitleContainer>
-      </Col>
-      <Col md={2}>
-        <Image
-          width="130"
-          rounded
-          src="https://maemresen.com/static/media/profile_pic.eb8d4952.jpg"
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col md={{
-        span: 8,
-        offset: 2,
-      }}
-      >
-        <Section className="mb-4">
-          I am an entrepreneur who wants to learn ne w technologies and use
-          them
-          to
-          make things easier. My life goal is having my own job and creating a
-          software community that everyone can get benefit from. I have deep
-          interest to Object Oriented Paradigm.
-        </Section>
-        <Section className="mb-4" primary>
-          <div>
-            Check My Cv here
-          </div>
-          <div>
-            English...Türkçe...
-          </div>
-        </Section>
-        <Section className="mb-4">
-          We have a software community named as YazilimVIP. Feel free to visit
-          our website by the following link
-        </Section>
-      </Col>
-    </Row>
+    <MainContainer className="p-4">
+      <Container>
+        <Row>
+          <Col md={2} xxl={3}>
+            <ImageContainer>
+              <Image
+                className="d-none d-lg-block"
+                width="130"
+                rounded
+                src="https://avatars2.githubusercontent.com/u/17517090?s=460&u=a0e442b6913f827d2e51e00abe143b2dbc375f33&v=4"
+              />
+            </ImageContainer>
+          </Col>
+          <Col md={8} xxl={6}>
+            <TitleContainer>
+              <Title>Mehmet Arif Emre Şen</Title>
+              <SubTitle>(a.k.a maemresen)</SubTitle>
+            </TitleContainer>
+          </Col>
+          <Col md={2} xxl={3}>
+            <ImageContainer>
+              <Image
+                className="d-none d-lg-block"
+                width="130"
+                rounded
+                src="https://maemresen.com/static/media/profile_pic.eb8d4952.jpg"
+              />
+            </ImageContainer>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 8, offset: 2 }} xxl={{ span: 6, offset: 3 }}>
+            <Section className="mt-4">
+              I am an entrepreneur who wants to learn ne w technologies and use
+              them
+              to
+              make things easier. My life goal is having my own job and creating a
+              software community that everyone can get benefit from. I have deep
+              interest to Object Oriented Paradigm.
+            </Section>
+            <Section className="mt-4" primary>
+              <h5>
+                <FontAwesomeIcon icon={faFile} />
+                <span className="mx-3">Check My Cv here</span>
+                <FontAwesomeIcon icon={faFile} />
+              </h5>
+              <CvContainer>
+                <h5 className="d-inline">
+                  <MaeLink
+                    target="_blank"
+                    href="https://cv.yazilim.vip/mehmet-arif-emre-sen.pdf"
+                  >
+                    English...
+                  </MaeLink>
+                </h5>
+                <h5 className="d-inline">
+                  <MaeLink
+                    target="_blank"
+                    href="https://cv.yazilim.vip/mehmet-arif-emre-sen-tr.pdf"
+                  >
+                    Türkçe...
+                  </MaeLink>
+                </h5>
+              </CvContainer>
+            </Section>
+            <Section className="mt-4">
+              We have a software community named as YazilimVIP. Feel free to visit
+              our website by the following link
+            </Section>
+            <Section className="mt-4">
+              <CvContainer>
+                <h5>
+                  <FontAwesomeIcon icon={faChampagneGlasses} />
+                  <MaeLink
+                    className="mx-3"
+                    target="_blank"
+                    href="https://www.yazilim.vip"
+                  >
+                    www.yazilim.vip
+                  </MaeLink>
+                  <FontAwesomeIcon icon={faChampagneGlasses} />
+                </h5>
+              </CvContainer>
+            </Section>
+          </Col>
+        </Row>
+      </Container>
+    </MainContainer>
   </Container>
 );
 
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -2,4 +2,5 @@
 export const COLORS = Object.freeze({
   PRIMARY: '#008000',
   SECONDARY: '#6c757d',
+  SECONDARY_DARK: '#494f54',
 });
