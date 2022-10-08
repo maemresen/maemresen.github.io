6422da08020ee747e242349195ef2d422cbc37ad
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2022-10-08 04:47:59 +0300

    init config and designs done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+import { Col, Container, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+import { COLORS } from './utils/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TitleContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+const Title = styled.span`
+  color: ${COLORS.PRIMARY}
+`;
+
+const App = () => (
+  <Container>
+    <Row>
+      <Col md={2}>
+        <Image
+          width="130"
+          rounded
+          src="https://avatars2.githubusercontent.com/u/17517090?s=460&u=a0e442b6913f827d2e51e00abe143b2dbc375f33&v=4"
+        />
+      </Col>
+      <Col md={8}>
+        <TitleContainer>
+          <Title>Mehmet Arif Emre Şen</Title>
+        </TitleContainer>
+      </Col>
+      <Col md={2}>
+        <Image
+          width="130"
+          rounded
+          src="https://maemresen.com/static/media/profile_pic.eb8d4952.jpg"
+        />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default App;
---
+++ src/utils/styles.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const COLORS = Object.freeze({
+  PRIMARY: "#008000",
+  SECONDARY: "#6c757d",
+})
